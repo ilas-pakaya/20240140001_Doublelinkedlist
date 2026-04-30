4b08f13093f4953d114770be6ec9c693cd7fecb5
MOJI <apple@apples-MacBook-Air.local>
2026-04-30 16:25:34 +0700

    create doublelinkedlist

---
+++ DoubleLinkedList.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+    int noMhs;
+    Node* next;
+    Node* prev;
+};
+
+class DoubleLinkedList {
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList() 
+    {
+        START = NULL;
+    }
+
